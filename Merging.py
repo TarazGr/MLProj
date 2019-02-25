f41c023d1e104d217d5aedf8f8120221292178cd
TarazGr <taranciucgabrielradu@yahoo.com>
2019-02-26 00:47:10 +0100

    Baby boy

--- Merging.py
+++ Merging.py
@@ -3,11 +3,11 @@ import numpy as np
 import pandas as pd
 
 spotify = pd.read_csv("spotifyAPIScraping.csv", encoding="latin1", usecols=["Artist", "Followers", "Popularity"],
-	names=["Artist", "Gen", "Followers", "Popularity"], header=0, na_values="ND").dropna(how="all")
+	names=["Artist", "Gen", "Followers", "Popularity"], header=0, na_values="ND").dropna(how="any")
 lastFM = pd.read_csv("lastFMAPIScraping.csv", encoding="latin1", usecols=["Track", "Artist", "Listeners", "Plays", "Duration"],
-	names=["Track", "Artist", "Listeners", "Plays", "Duration", "Gen"], header=0, na_values="ND").dropna(how="all")
+	names=["Track", "Artist", "Listeners", "Plays", "Duration", "Gen"], header=0, na_values="ND").dropna(how="any")
 
-dataset = pd.merge(spotify, lastFM, on="Artist", how="outer")
+dataset = pd.merge(spotify, lastFM, on="Artist", how="outer").dropna(how="any")
 
 dataset.to_csv("merged.csv", encoding="latin1", index=False)
 
