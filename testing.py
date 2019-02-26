176d00f92a9e30804cad9fb908e046f88f3769d7
TarazGr <taranciucgabrielradu@yahoo.com>
2019-02-26 23:19:24 +0100

    life's good

--- testing.py
+++ testing.py
@@ -4,17 +4,17 @@ import numpy as np
 import pandas as pd
 
 
-def NDChecker(value):
-    toCheck = [3, 4, 5, 6, 7]
-    for number in toCheck:
+def nd_checker(value):
+    to_check = [3, 4, 5, 6, 7]
+    for number in to_check:
         if value[number] is None or value[number] == "ND":
             return True
     return False
 
 
-def nanchecker(value):
-    toCheck = [3, 4, 5, 6, 7]
-    for number in toCheck:
+def nan_checker(value):
+    to_check = [3, 4, 5, 6, 7]
+    for number in to_check:
         if math.isnan(float(value[number])) or float(value[number]) is None:
             return True
     return False
@@ -26,9 +26,8 @@ dt = pd.read_csv('merged.csv', header=0)
 dtEur = pd.read_csv('mergedEur.csv', header=0)
 
 for i, row in enumerate(dt.itertuples()):
-    if row[3] == 'Followers' or row[3] == 'followers' or NDChecker(row):
+    if row[3] == 'Followers' or row[3] == 'followers' or nd_checker(row):
         continue
-    print(row[3])
     toAppend = [float(row[3]), float(row[4]), float(row[5]), float(row[6]), float(row[7])]
     trainingSet.append(toAppend)
     testSet.append(row[8])
@@ -44,7 +43,7 @@ y = len(trainingSet)
 clf = DecisionTreeClassifier()
 
 
-clf.fit(trainingSet,testSet)
-test = [31295.0,54.0,23700.0,219700.0,247000.0]
+clf.fit(trainingSet, testSet)
+test = [31295.0, 54.0, 23700.0, 219700.0, 247000.0]
 risultato = clf.predict([test])
 print("e quindi")
