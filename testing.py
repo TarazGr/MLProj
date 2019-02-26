b545508dd190d73e9d54b662ec68448d02572b58
TarazGr <taranciucgabrielradu@yahoo.com>
2019-02-26 23:31:06 +0100

    QoL changes

--- testing.py
+++ testing.py
@@ -26,15 +26,11 @@ dt = pd.read_csv('merged.csv', header=0)
 dtEur = pd.read_csv('mergedEur.csv', header=0)
 
 for i, row in enumerate(dt.itertuples()):
-    if row[3] == 'Followers' or row[3] == 'followers' or nd_checker(row):
-        continue
     toAppend = [float(row[3]), float(row[4]), float(row[5]), float(row[6]), float(row[7])]
     trainingSet.append(toAppend)
     testSet.append(row[8])
 
 for i, row in enumerate(dtEur.itertuples()):
-    if row[3] == 'Followers' or row[3] == 'followers' or i == 1:
-        continue
     toAppend = [float(row[3]), float(row[4]), float(row[5]), float(row[6]), float(row[7])]
     trainingSet.append(toAppend)
     testSet.append(float(row[8]))
