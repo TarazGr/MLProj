5041c6e40387954ce19b97f925448f7db8e5c28c
Gianmarco Forcella <gimmi@MacBook-Pro.lan>
2019-03-07 23:55:05 +0100

    Aggiunta di un dataset perfettamente bilanciato (cit)

--- DecisionTree.py
+++ DecisionTree.py
@@ -7,21 +7,14 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from joblib import dump, load
 
-data = pd.read_csv("datasetFINAL.csv", encoding="latin1", header=0)
-
-features = data[["danceability", "energy", "loudness", "speechiness", "acousticness", "instrumentalness", "liveness",
-                 "key", "mode", "duration"]]
+data = pd.read_csv('datasetFINAL.csv', encoding='UTF-8')
+features = data[["danceability","energy","loudness","speechiness","acousticness","instrumentalness","liveness","key","mode","duration"]]
 target = data["billboarder"]
 
 train_features, test_features, train_target, test_target = train_test_split(features, target)
-
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(train_features, train_target)
-
 predictions = clf.predict(test_features)
-
 print(confusion_matrix(test_target, predictions))
-
 print(classification_report(test_target, predictions))
-
 dump(clf, "DTree_Model")
