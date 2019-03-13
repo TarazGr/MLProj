de4e12bc84fea73c9aed353680f7c65e10d22394
TarazGr <taranciucgabrielradu@yahoo.com>
2019-03-13 18:19:21 +0100

    normalization code

---
+++ Scrapers/just_temporary.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+df = pd.read_csv("SUPERTEST.csv", encoding="latin1", header=None,
+	names=["acousticness", "artist", "billboarder", "danceability", "duration", "energy", "instrumentalness", "key", "liveness", "loudness", "mode", "speechiness", "title", "followers", "popularity"], 
+	usecols=["acousticness", "billboarder", "danceability", "duration", "energy", "instrumentalness", "key", "liveness", "loudness", "mode", "speechiness", "followers", "popularity"])
+
+to_scale = df[["loudness", "followers", "popularity"]]
+
+scaled = MinMaxScaler().fit_transform(to_scale)
+
+df["loudness"] = scaled[:, 0]
+df["followers"] = scaled[:, 1]
+df["popularity"] = scaled[:, 2]
+
+print(df.describe())
